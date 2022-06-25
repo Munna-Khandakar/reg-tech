8f615006e691bff9aa75fa1da6501ab96593d02e
Munna-Khandakar <munnashisad@gmail.com>
2022-06-25 14:11:19 +0600

    env added

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-  MONGO_URI:
-    "mongodb+srv://munna:Shisad807577@cluster0.nxkrhrb.mongodb.net/REGISTRATIONFORM?retryWrites=true&w=majority",
+  MONGO_URI: process.env.MONGO_URI,
   SECRET_KEY: "techsimpict-munna",
 };
 
